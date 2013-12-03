15b7c99daacaab3f9288ee988963eb16d06cb4c3
Radu Potop <radu.potop@wooptoo.com>
2013-12-03 15:28:42 +0200

    basic markdown reader

--- blag.js
+++ blag.js
@@ -1,3 +1,27 @@
 var markdown = require('markdown').markdown;
+var fs = require('fs');
 
-console.log(markdown.toHTML( "Hello *World*!" ));
+var contentDir = 'content/';
+var outputDir = 'output/';
+
+/**
+ * Read content filed
+ */
+fs.readdir(contentDir, function(err, files) {
+    files.forEach(function(filename) {
+        if((/\.md$/).test(filename)) {
+            fs.readFile(contentDir + filename, 'utf8', function(err, fileContent) {
+                //~console.log(fileContent);
+                var htmlContent = renderHtml(fileContent);
+                fs.writeFile(outputDir + filename.replace('.md', '.html'), htmlContent);
+            });
+        }
+    });
+});
+
+/**
+ * Render MD as HTML
+ */
+function renderHtml(mdContent) {
+   return markdown.toHTML(mdContent);
+}
